b9f887dad2b27e02806abe4a66d291449687859a
Connie Leung <cleung334@gmail.com>
2024-02-07 00:11:56 +0800

    feat: add styles

--- src/app/forms/control-wrapper/control-wrapper.component.ts
+++ src/app/forms/control-wrapper/control-wrapper.component.ts
@@ -9,7 +9,26 @@ import { Component } from '@angular/core';
       control-wrapper works!
     </p>
   `,
-  styles: ``
+  styles: `
+    .input-wrapper__errors {
+      margin-top: 8px;
+      margin-left: 150px;
+      width:200px;
+      li {
+        list-style-type: none;
+        color: #ff0000;
+      }
+      ul {
+        padding: 0px;
+        margin: 0px;
+      }
+    }
+    :host {
+      display: flex;
+      flex-direction: column;
+      padding-bottom: 8px;
+    }
+  `
 })
 export class ControlWrapperComponent {
 
