07b6717dd0295546771507370996dee97d17fc86
Connie Leung <cleung334@gmail.com>
2024-02-06 23:51:44 +0800

    feat: add control wrapper

---
+++ src/app/forms/control-wrapper/control-wrapper.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-wrapper',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      control-wrapper works!
+    </p>
+  `,
+  styles: ``
+})
+export class ControlWrapperComponent {
+
+}
